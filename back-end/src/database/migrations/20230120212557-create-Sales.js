11f1604733c502a4bcd72c8df161420c78042924
Juliana Alvares <alvares.juliana@hotmail.com>
2023-01-20 20:06:07 -0300

    database config

---
+++ back-end/src/database/migrations/20230120212557-create-Sales.js
@@ -0,0 +1,66 @@
+'use strict';
+
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.createTable('sales', {
+      id: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+      },
+      userId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'user_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      sellerId: {
+        type: Sequelize.INTEGER,
+        allowNull: false,
+        field: 'seller_id',
+        onUpdate: 'CASCADE',
+        onDelete: 'CASCADE',
+        foreignKey: true,
+        references: {
+          model: 'users',
+          key: 'id',
+        },
+      },
+      totalPrice: {
+        type: Sequelize.DECIMAL(9, 2),
+        allowNull: false,
+        field: 'total_price',
+      },
+      deliveryAddress: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'delivery_address',
+      },
+      deliveryNumber: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        field: 'delivery_number',
+      },
+      saleDate: {
+        type: Sequelize.DATE,
+        allowNull: false,
+        field: 'sale_date',
+      },
+      status: {
+        type: Sequelize.STRING,
+        allowNull: false,
+      }
+    });
+  },
+
+  async down (queryInterface) {
+    await queryInterface.dropTable('sales');
+  }
+};
