d594dfb148675a87851b83cca2e1943b9cff7c75
Edmilson Corrêa <edm.correa@hotmail.com>
2023-02-01 15:05:54 -0300

    first commit and create file SellerOrderDDetail

---
+++ front-end/src/pages/SellerOrderDetail.js
@@ -0,0 +1,15 @@
+import NavBar from '../components/navbar';
+import OrdersUserDetailsComponent from '../components/orders.user.details.';
+import CheckoutComponent from '../components/products.checkout';
+
+function OrdersDetail() {
+  return (
+    // <div>
+    //   <NavBar />
+    //   <OrdersUserDetailsComponent />
+    //   <CheckoutComponent dataTest="order_details" />
+    // </div>
+  );
+}
+
+export default OrdersDetail;
