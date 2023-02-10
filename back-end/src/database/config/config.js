37e94fef30f810ab586bfb8feabd57730449043b
Edmilson Corrêa <edm.correa@hotmail.com>
2023-02-10 18:33:49 -0300

    adding .env config

--- back-end/src/database/config/config.js
+++ back-end/src/database/config/config.js
@@ -1,23 +1,21 @@
 require('dotenv').config();
 
-// const environment = process.env.NODEENV || "test";
+const environment = process.env.NODEENV || "test";
 
-// const suffix = {
-//   prod: "",
-//   production: "",
-//   dev: "-dev",
-//   development: "-dev",
-//   test: "-test",
-// };
+const suffix = {
+  prod: "",
+  production: "",
+  dev: "-dev",
+  development: "-dev",
+  test: "-test",
+};
 
 const options = {
-  host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
-  port: process.env.MYSQLPORT || '3306',
-  database: process.env.MYSQLDBNAME || process.env.MYSQLDATABASE,
-    // database: 
-    // `${process.env.MYSQLDBNAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
-  username: process.env.MYSQLUSER || 'root',
-  password: process.env.MYSQLPASSWORD || 'password',
+  host: process.env.MYSQLHOST ,
+  port: process.env.MYSQLPORT,
+  database: process.env.MYSQLDATABASE,
+  username: process.env.MYSQLUSER,
+  password: process.env.MYSQLPASSWORD,
   dialect: 'mysql',
   dialectOptions: {
     timezone: 'Z',
@@ -36,3 +34,16 @@ module.exports = {
     ...options,
   },
 };
+
+// host: process.env.HOSTNAME || process.env.MYSQLHOST || 'localhost',
+// port: process.env.MYSQLPORT || '3306',
+// database: process.env.MYSQLDBNAME || process.env.MYSQLDATABASE,
+//   // database: 
+//   // `${process.env.MYSQLDBNAME || 'delivery-app'}${suffix[environment] || suffix.test}`,
+// username: process.env.MYSQLUSER || 'root',
+// password: process.env.MYSQLPASSWORD || 'password',
+// dialect: 'mysql',
+// dialectOptions: {
+//   timezone: 'Z',
+// },
+// logging: false,
